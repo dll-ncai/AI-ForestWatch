51c8cdd59fe1705170ea5eb4d557131a2e08bd2b
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-11-21 15:32:37 +0500

    Update run_all_inferences.sh

--- run_all_inferences.sh
+++ run_all_inferences.sh
@@ -2,7 +2,7 @@
 # All rights reserved.
 
 # This source code is licensed under the MIT license found in the
-# LICENSE.md file in the root directory of this source tree.
+# LICENSE file in the root directory of this source tree.
 
 #!/usr/bin/env bash
 for year in `seq 2016 2017`;
