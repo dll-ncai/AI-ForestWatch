c79bcd6b29241e271aa59dded310e5f7d75bf252
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-11-21 15:32:54 +0500

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -2,7 +2,7 @@
 # All rights reserved.
 
 # This source code is licensed under the MIT license found in the
-# LICENSE.md file in the root directory of this source tree.
+# LICENSE file in the root directory of this source tree.
 
 lr="1e-06"
 topology="ENC_4_DEC_4"
