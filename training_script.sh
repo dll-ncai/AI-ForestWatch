fc663c1a75e7b3855ff663af67b6544bfc050621
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-11-21 15:32:17 +0500

    Update training_script.sh

--- training_script.sh
+++ training_script.sh
@@ -2,7 +2,7 @@
 # All rights reserved.
 
 # This source code is licensed under the MIT license found in the
-# LICENSE.md file in the root directory of this source tree.
+# LICENSE file in the root directory of this source tree.
 
 #!/bin/bash
 
