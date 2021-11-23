988397cc453ae32b28f8b96f018f136b685c9547
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-11-23 13:18:36 +0500

    Add testing logic / remove tensorboard writer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -24,11 +24,14 @@ class Trainer(BaseTrainer):
             self.len_epoch = len_epoch
         self.valid_data_loader = valid_data_loader
         self.do_validation = self.valid_data_loader is not None
+        self.test_data_loader = valid_data_loader
+        self.do_test = self.test_data_loader is not None
         self.lr_scheduler = lr_scheduler
         self.log_step = int(np.sqrt(data_loader.batch_size))
 
         self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns])
         self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns])
+        self.test_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns])
 
     def _train_epoch(self, epoch):
         """
@@ -48,7 +51,6 @@ class Trainer(BaseTrainer):
             loss.backward()
             self.optimizer.step()
 
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update('loss', loss.item())
             for met in self.metric_ftns:
                 self.train_metrics.update(met.__name__, met(output, target))
@@ -64,18 +66,17 @@ class Trainer(BaseTrainer):
         log = self.train_metrics.result()
 
         if self.do_validation:
-            val_log = self._valid_epoch(epoch)
+            val_log = self._valid_epoch()
             log.update(**{'val_'+k : v for k, v in val_log.items()})
 
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
         return log
 
-    def _valid_epoch(self, epoch):
+    def _valid_epoch(self):
         """
         Validate after training an epoch
 
-        :param epoch: Integer, current training epoch.
         :return: A log that contains information about validation
         """
         self.model.eval()
@@ -87,11 +88,31 @@ class Trainer(BaseTrainer):
                 output = self.model(data)
                 loss = self.criterion(output, target)
 
-                self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
                 self.valid_metrics.update('loss', loss.item())
                 for met in self.metric_ftns:
                     self.valid_metrics.update(met.__name__, met(output, target))
         return self.valid_metrics.result()
+    
+    def _test_epoch(self):
+        """
+        Test after training
+
+        :return: A log that contains information about testing
+        """
+        self.model.eval()
+        self.test_metrics.reset()
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(self.test_data_loader):
+                data, target = data.to(self.device), target.to(self.device)
+
+                output = self.model(data)
+                loss = self.criterion(output, target)
+
+                self.test_metrics.update('loss', loss.item())
+                for met in self.metric_ftns:
+                    self.test_metrics.update(met.__name__, met(output, target))
+        return self.test_metrics.result()
+    
 
     def _progress(self, batch_idx):
         base = '[{}/{} ({:.0f}%)]'
