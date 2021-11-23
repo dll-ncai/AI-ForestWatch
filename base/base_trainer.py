9e0c51fc4363a986b32c2e32d1f7b8734fa5cc4e
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-11-23 13:21:58 +0500

    Update base_trainer.py

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -48,6 +48,14 @@ class BaseTrainer:
         :param epoch: Current epoch number
         """
         raise NotImplementedError
+        
+        
+    @abstractmethod
+    def _test_epoch(self):
+        """
+        Testing logic after last epoch
+        """
+        raise NotImplementedError
 
     def train(self):
         """
@@ -92,6 +100,10 @@ class BaseTrainer:
 
             if epoch % self.save_period == 0:
                 self._save_checkpoint(epoch, save_best=best)
+                
+            if epoch == self.epochs:
+                test_log = self._test_epoch()
+                log.update(**{'test_'+k : v for k, v in test_log.items()})
 
     def _save_checkpoint(self, epoch, save_best=False):
         """
