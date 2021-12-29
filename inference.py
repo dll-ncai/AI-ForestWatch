ffbbdd4e996f4563d64dcc003ffe03d1b5a508c6
Qasim Khan <straightouttadetroit.qk@gmail.com>
2021-12-29 14:02:03 +0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -7,6 +7,8 @@
 import argparse
 import torch
 from tqdm import tqdm
+import matplotlib.image as matimg
+
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
@@ -53,8 +55,40 @@ def main(config):
     else:
         # TODO
         # get all years in directory
+        
+    for district in all_districts:
+        for year in years:
+            print("(LOG): On District: {} @ Year: {}".format(district, year))
+            image_path = os.path.join(args.data_path, 'landsat8_{}_region_{}.tif'.format(year, district)) #added(nauman)
+            inference_loader = config.init_obj('inference_data_loader', module_data, image_path)
+            adjustment_mask = inference_loader.dataset.adjustment_mask
+            # we need to fill our new generated test image
+            generated_map = np.empty(shape=inference_loader.dataset.get_image_size())
+            for idx, data in enumerate(inference_loader):
+                coordinates, test_x = data['coordinates'].tolist(), data['input']
+                test_x = test_x.cuda(device=args.device) if args.cuda else test_x
+                out_x, softmaxed = model.forward(test_x)
+                pred = torch.argmax(softmaxed, dim=1)
+                pred_numpy = pred.cpu().numpy().transpose(1,2,0)
+                if idx % 5 == 0:
+                    print('LOG: on {} of {}'.format(idx, len(inference_loader)))
+                for k in range(test_x.shape[0]):
+                    x, x_, y, y_ = coordinates[k]
+                    generated_map[x:x_, y:y_] = pred_numpy[:,:,k]
+            # adjust the inferred map
+            generated_map += 1  # to make forest pixels: 2, non-forest pixels: 1, null pixels: 0
+            generated_map = np.multiply(generated_map, adjustment_mask)
+            # save generated map as png image, not numpy array
+            forest_map_rband = np.zeros_like(generated_map)
+            forest_map_gband = np.zeros_like(generated_map)
+            forest_map_bband = np.zeros_like(generated_map)
+            forest_map_gband[generated_map == FOREST_LABEL] = 255
+            forest_map_rband[generated_map == NON_FOREST_LABEL] = 255
+            forest_map_for_visualization = np.dstack([forest_map_rband, forest_map_gband, forest_map_bband]).astype(np.uint8)
+            save_this_map_path = os.path.join(args.dest, '{}_{}_inferred_map.png'.format(district, year))
+            matimg.imsave(save_this_map_path, forest_map_for_visualization)
+            print('Saved: {} @ {}'.format(save_this_map_path, forest_map_for_visualization.shape))
     
-
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='Inference Script')
